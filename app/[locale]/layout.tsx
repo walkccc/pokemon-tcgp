8f4c21ac16e10752f2244a08efb370435e50c593
Peng-Yu Chen <me@pengyuc.com>
2024-12-25 23:37:11 -0500

    fix: wrap `params` with `Promise`

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -30,7 +30,7 @@ export default async function LocaleLayout({
   params,
 }: Readonly<{
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }>) {
   const { locale } = await params;
 
